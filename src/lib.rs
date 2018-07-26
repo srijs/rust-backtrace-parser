f665d9af44fbb54a45278a495045d301dda5eac7
Sam Rijs <srijs@airpost.net>
2018-07-26 16:33:14 +1000

    convert into pull parser

--- src/lib.rs
+++ src/lib.rs
@@ -2,41 +2,142 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use std::error::Error;
+use std::fmt;
 use std::path::Path;
 
-mod parser;
-pub use self::parser::{parse, ParseError};
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct BacktraceParser;
+
+const _GRAMMAR: &str = include_str!("grammar.pest");
 
 #[derive(Debug)]
-pub struct ParsedBacktrace<'a> {
-    frames: Vec<ParsedFrame<'a>>,
+pub struct ParseError<'a> {
+    inner: pest::Error<'a, Rule>,
 }
 
-impl<'a> ParsedBacktrace<'a> {
-    pub fn frames(&self) -> &[ParsedFrame<'a>] {
-        &self.frames
+impl<'a> fmt::Display for ParseError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&self.inner, f)
+    }
+}
+
+impl<'a> Error for ParseError<'a> {}
+
+#[derive(Debug)]
+pub struct Backtrace<'a> {
+    pairs: pest::iterators::Pairs<'a, Rule>,
+}
+
+impl<'a> Backtrace<'a> {
+    pub fn parse(input: &'a str) -> Result<Backtrace<'a>, ParseError<'a>> {
+        let pairs = BacktraceParser::parse(Rule::backtrace, input)
+            .map_err(|err| ParseError { inner: err })?;
+
+        Ok(Backtrace { pairs })
+    }
+}
+
+impl<'a> Backtrace<'a> {
+    pub fn into_frames(self) -> Frames<'a> {
+        Frames { inner: self.pairs }
+    }
+}
+
+pub struct Frames<'a> {
+    inner: pest::iterators::Pairs<'a, Rule>,
+}
+
+impl<'a> Iterator for Frames<'a> {
+    type Item = Frame<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(frame) = self.inner.next() {
+            debug_assert!(frame.as_rule() == Rule::frame);
+            let mut frame_inner = frame.into_inner();
+
+            let frame_index = frame_inner.next().unwrap();
+            debug_assert!(frame_index.as_rule() == Rule::frame_index);
+            let frame_pointer = frame_inner.next().unwrap();
+            debug_assert!(frame_pointer.as_rule() == Rule::frame_pointer);
+
+            Some(Frame { pairs: frame_inner })
+        } else {
+            None
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct ParsedFrame<'a> {
-    symbols: Vec<ParsedSymbol<'a>>,
+pub struct Frame<'a> {
+    pairs: pest::iterators::Pairs<'a, Rule>,
 }
 
-impl<'a> ParsedFrame<'a> {
-    pub fn symbols(&self) -> &[ParsedSymbol<'a>] {
-        &self.symbols
+impl<'a> Frame<'a> {
+    pub fn into_symbols(self) -> Symbols<'a> {
+        Symbols { inner: self.pairs }
+    }
+}
+
+pub struct Symbols<'a> {
+    inner: pest::iterators::Pairs<'a, Rule>,
+}
+
+impl<'a> Iterator for Symbols<'a> {
+    type Item = Symbol<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(symbol) = self.inner.next() {
+            match symbol.as_rule() {
+                Rule::symbol_non_empty => {
+                    let mut parsed_symbol = Symbol {
+                        name: None,
+                        filename: None,
+                        lineno: None,
+                    };
+                    let mut symbol_inner = symbol.into_inner();
+                    let symbol_name = symbol_inner.next().unwrap();
+                    match symbol_name.as_rule() {
+                        Rule::symbol_name_known => {
+                            parsed_symbol.name = Some(symbol_name.into_span().as_str())
+                        }
+                        _ => {}
+                    }
+                    if let Some(symbol_location) = symbol_inner.next() {
+                        debug_assert!(symbol_location.as_rule() == Rule::symbol_location);
+                        let mut symbol_location_inner = symbol_location.into_inner();
+                        let symbol_location_path = symbol_location_inner.next().unwrap();
+                        debug_assert!(symbol_location_path.as_rule() == Rule::symbol_location_path);
+                        parsed_symbol.filename =
+                            Some(Path::new(symbol_location_path.into_span().as_str()));
+                        let symbol_location_lineno = symbol_location_inner.next().unwrap();
+                        debug_assert!(
+                            symbol_location_lineno.as_rule() == Rule::symbol_location_lineno
+                        );
+                        parsed_symbol.lineno =
+                            Some(symbol_location_lineno.into_span().as_str().parse().unwrap());
+                    }
+                    Some(parsed_symbol)
+                }
+                _ => None,
+            }
+        } else {
+            None
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct ParsedSymbol<'a> {
+pub struct Symbol<'a> {
     name: Option<&'a str>,
     filename: Option<&'a Path>,
     lineno: Option<u32>,
 }
 
-impl<'a> ParsedSymbol<'a> {
+impl<'a> Symbol<'a> {
     pub fn name(&self) -> Option<&'a str> {
         self.name
     }
--- tests/parse.rs
+++ tests/parse.rs
@@ -2,48 +2,91 @@ extern crate backtrace_parser;
 
 use std::path::Path;
 
-use backtrace_parser::parse;
+use backtrace_parser::Backtrace;
 
 #[test]
 fn unresolved_symbols() {
     let data = include_str!("fixtures/unresolved.txt");
-    let parsed = parse(data).unwrap();
+    let parsed = Backtrace::parse(data).unwrap();
 
-    assert_eq!(parsed.frames().len(), 1);
-    assert_eq!(parsed.frames()[0].symbols().len(), 0);
+    let mut frames = parsed.into_frames();
+
+    let symbols = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols.len(), 0);
+
+    assert!(frames.next().is_none());
 }
 
 #[test]
 fn no_symbol_info() {
     let data = include_str!("fixtures/no-info.txt");
-    let parsed = parse(data).unwrap();
+    let parsed = Backtrace::parse(data).unwrap();
 
-    assert_eq!(parsed.frames().len(), 1);
-    assert_eq!(parsed.frames()[0].symbols().len(), 0);
+    let mut frames = parsed.into_frames();
+
+    let symbols = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols.len(), 0);
+
+    assert!(frames.next().is_none());
 }
 
 #[test]
 fn full_backtrace() {
     let data = include_str!("fixtures/full.txt");
-    let parsed = parse(data).unwrap();
+    let parsed = Backtrace::parse(data).unwrap();
 
-    assert_eq!(parsed.frames().len(), 13);
+    let mut frames = parsed.into_frames();
 
-    assert_eq!(parsed.frames()[0].symbols().len(), 2);
+    let symbols0 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols0.len(), 2);
     assert_eq!(
-        parsed.frames()[0].symbols()[0].name(),
+        symbols0[0].name(),
         Some("backtrace::backtrace::libunwind::trace::h042fc201d46ac6bb")
     );
-    assert_eq!(parsed.frames()[0].symbols()[0].filename(), Some(Path::new("/root/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.9/src/backtrace/libunwind.rs")));
-    assert_eq!(parsed.frames()[0].symbols()[0].lineno(), Some(53));
+    assert_eq!(symbols0[0].filename(), Some(Path::new("/root/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.9/src/backtrace/libunwind.rs")));
+    assert_eq!(symbols0[0].lineno(), Some(53));
     assert_eq!(
-        parsed.frames()[0].symbols()[1].name(),
+        symbols0[1].name(),
         Some("backtrace::backtrace::trace::hd8156e10e3d1f9ca")
     );
-    assert_eq!(parsed.frames()[0].symbols()[1].filename(), Some(Path::new("/root/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.9/src/backtrace/mod.rs")));
-    assert_eq!(parsed.frames()[0].symbols()[1].lineno(), Some(42));
+    assert_eq!(symbols0[1].filename(), Some(Path::new("/root/.cargo/registry/src/github.com-1ecc6299db9ec823/backtrace-0.3.9/src/backtrace/mod.rs")));
+    assert_eq!(symbols0[1].lineno(), Some(42));
 
-    assert_eq!(parsed.frames()[11].symbols()[0].name(), Some("_start"));
+    let symbols1 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols1.len(), 1);
 
-    assert_eq!(parsed.frames()[12].symbols()[0].name(), None);
+    let symbols2 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols2.len(), 1);
+
+    let symbols3 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols3.len(), 1);
+
+    let symbols4 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols4.len(), 1);
+
+    let symbols5 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols5.len(), 2);
+
+    let symbols6 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols6.len(), 1);
+
+    let symbols7 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols7.len(), 3);
+
+    let symbols8 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols8.len(), 1);
+
+    let symbols9 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols9.len(), 1);
+
+    let symbols10 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols10.len(), 1);
+
+    let symbols11 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols11[0].name(), Some("_start"));
+
+    let symbols12 = frames.next().unwrap().into_symbols().collect::<Vec<_>>();
+    assert_eq!(symbols12[0].name(), None);
+
+    assert!(frames.next().is_none());
 }
